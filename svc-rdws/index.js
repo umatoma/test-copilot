9940be9164bd9bd7c0df7cf943248fd724daff14
umatoma <umatomakun@gmail.com>
2021-11-23 23:18:16 +0900

    update

--- svc-rdws/index.js
+++ svc-rdws/index.js
@@ -1,25 +1,42 @@
 const process = require('process');
 const express = require('express');
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
 const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
 
+const appName = process.env.COPILOT_APPLICATION_NAME;
+const envName = process.env.COPILOT_ENVIRONMENT_NAME;
+const svcName = process.env.COPILOT_SERVICE_NAME;
+
 const app = express();
-const port = 3000;
+const port = 3000;''
 const wrap = (fn) => (...args) => fn(...args).catch(args[2]);
 
-const client = new SNSClient({ region: 'ap-northeast-1' });
+const ddbClient = new DynamoDBClient({ region: 'ap-northeast-1' });
+const snsClient = new SNSClient({ region: 'ap-northeast-1' });
 const { helloTopic } = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.get('/send', wrap(async (req, res) => {
-  const out = await client.send(new PublishCommand({
-    Message: 'HELLO',
+  const body = `HELLO_${Date.now()}`
+  const data = await ddbClient.send(new PutItemCommand({
+    TableName: `${appName}-${envName}-${svcName}-messages`,
+    Item: {
+      id: { S: `${Date.now()}` },
+      body: { S: body },
+    },
+  }));
+  console.log('data', JSON.stringify(data));
+
+  const out = await snsClient.send(new PublishCommand({
+    Message: body,
     TopicArn: helloTopic,
   }));
   res.json({
     helloTopic,
     out,
+    data,
   });
 }));
 app.use((err, req, res, next) => {
